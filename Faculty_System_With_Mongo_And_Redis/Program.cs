ad77cdce2148847d11911e66046c3ebdf23e6130
shady00s <ssk2310@hotmail.com>
2022-06-16 20:40:39 +0200

    Add project files.

---
+++ Faculty_System_With_Mongo_And_Redis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Faculty_System_With_Mongo_And_Redis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
